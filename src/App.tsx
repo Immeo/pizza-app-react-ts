6e99880418e78d989084251903735998bf7a5cbc
immeo <wayfarer245@gmail.com>
2024-08-02 08:41:12 +0300

    03-added an example of function typing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import { useEffect, useState } from 'react';
 import MaimButton from './components/MainButton/MainButton';
 
 function App() {
-	return <MaimButton onClick={() => console.log('tst')}>Button</MaimButton>;
+	const [counter, setCounter] = useState<number>(0);
+	const increment = () => {
+		setCounter(prev => prev + 1);
+	};
+	useEffect(() => {
+		// increment();
+		console.log('counter', counter);
+	}, [counter]);
+
+	return (
+		<>
+			<MaimButton onClick={() => increment()}>Button</MaimButton>;
+			<div>{counter}</div>
+		</>
+	);
 }
 export default App;
