2e9d9fac36c32a00afe06ff05acba97a575ca5dc
immeo <wayfarer245@gmail.com>
2024-08-03 15:55:41 +0300

    08-add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
 import Input from './components/InputForm/Input';
 import MaimButton from './components/MainButton/MainButton';
+import Cart from './pages/Cart/Cart';
+import Error from './pages/Error/Error';
+import Menu from './pages/Menu/Menu';
 
 function App() {
 	return (
 		<>
+			<header>
+				<nav>
+					<a href='/'>Menu</a>
+					<a href='/cart'>Cart</a>
+				</nav>
+			</header>
 			<Input placeholder='Email' />
 			<MaimButton appearence='big'>siign</MaimButton>
+			<Routes>
+				<Route path='/' element={<Menu />} />
+				<Route path='/cart' element={<Cart />} />
+				<Route path='*' element={<Error />} />
+			</Routes>
 		</>
 	);
 }
