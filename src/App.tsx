d22eefdcef1fafba8326bf743a0f40eb9f77139e
immeo <wayfarer245@gmail.com>
2024-08-04 12:23:14 +0300

    09-update method route

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import { Route, Routes } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Input from './components/InputForm/Input';
 import MaimButton from './components/MainButton/MainButton';
 import Cart from './pages/Cart/Cart';
 import Error from './pages/Error/Error';
 import Menu from './pages/Menu/Menu';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Menu />
+	},
+	{
+		path: '/cart',
+		element: <Cart />
+	},
+	{
+		path: '*',
+		element: <Error />
+	}
+]);
 
 function App() {
 	return (
@@ -16,11 +31,7 @@ function App() {
 			</header>
 			<Input placeholder='Email' />
 			<MaimButton appearence='big'>siign</MaimButton>
-			<Routes>
-				<Route path='/' element={<Menu />} />
-				<Route path='/cart' element={<Cart />} />
-				<Route path='*' element={<Error />} />
-			</Routes>
+			<RouterProvider router={router} />
 		</>
 	);
 }
