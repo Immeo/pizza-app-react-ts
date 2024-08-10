ba70983387044c8433d8db144b2cdb2aada88033
immeo <wayfarer245@gmail.com>
2024-08-10 18:00:38 +0300

    16-add ProductCard

---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,39 @@
+import { Link } from 'react-router-dom';
+import styles from './ProductCard.module.css';
+import { ProductCardProps } from './ProductCard.props';
+
+function ProductCard(props: ProductCardProps) {
+	const priceToUSDFormat = new Intl.NumberFormat('en-US', {
+		style: 'currency',
+		currency: 'USD'
+	}).format(props.price);
+	const trueFormat = priceToUSDFormat.slice(1);
+
+	return (
+		<Link to='/' className={styles['link']}>
+			<div className={styles['card']}>
+				<div
+					className={styles['head']}
+					style={{ backgroundImage: `url(${props.image})` }}
+				>
+					<div className={styles['price']}>
+						<span className={styles['currency']}>$</span>
+						{trueFormat}
+					</div>
+					<button type='button' className={styles['add-to-cart']}>
+						<img src='/cartBtn.svg' alt='Add to cart' />
+					</button>
+					<div className={styles['rating']}>
+						{props.rating}
+						<img src='/starIcon.svg' alt='Raiting icon' />
+					</div>
+				</div>
+				<div className={styles['footer']}>
+					<div className={styles['title']}>{props.title}</div>
+					<div className={styles['description']}>{props.description}</div>
+				</div>
+			</div>
+		</Link>
+	);
+}
+export default ProductCard;
--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,4 +1,5 @@
 import Headling from '../../components/Headling/Headling';
+import ProductCard from '../../components/ProductCard/ProductCard';
 import Search from '../../components/Search/Search';
 import styles from './Menu.module.css';
 
@@ -8,6 +9,16 @@ function Menu() {
 			<div className={styles['head']}>
 				<Headling>Menu</Headling>
 				<Search placeholder='Enter the dish or composition' />
+			</div>
+			<div>
+				<ProductCard
+					id={1}
+					image='/examplePizzaImg.png'
+					title='Enjoyment'
+					rating={5}
+					description='Salami, arugula, tomatoes, olives'
+					price={5}
+				/>
 			</div>
 		</>
 	);
