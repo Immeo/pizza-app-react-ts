ab290ce06fa0d83607b084bbf1070840308e23b6
immeo <wayfarer245@gmail.com>
2024-08-15 13:41:04 +0300

    28-added a design to the login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,11 +1,32 @@
+import { FormEvent } from 'react';
+import { Link } from 'react-router-dom';
+import Layout from '../../components/Headling/Headling';
+import Input from '../../components/InputForm/Input';
+import MaimButton from '../../components/MainButton/MainButton';
+import styles from './Login.module.css';
+
 export function Login() {
+	const onSubmit = (event: FormEvent) => {
+		event.preventDefault();
+	};
+
 	return (
-		<div>
-			<h1>Login</h1>
-			<form>
-				<input type='text' placeholder='Username' />
-				<input type='password' placeholder='Password' />
-				<button type='submit'>Login</button>
+		<div className={styles['login']}>
+			<Layout>Вход</Layout>
+			<form action='' className={styles['form']} onSubmit={onSubmit}>
+				<div className={styles['field']}>
+					<label htmlFor='email'>Ваш email</label>
+					<Input id='email' placeholder='Email' />
+				</div>
+				<div className={styles['field']}>
+					<label htmlFor='password'>Ваш пароль</label>
+					<Input id='password' type='password' placeholder='Пароль' />
+				</div>
+				<MaimButton appearence='big'>Вход</MaimButton>
+				<div>Нет аккаунта?</div>
+				<Link to={'/auth/register'} className={styles['links']}>
+					Зарегистрироваться?
+				</Link>
 			</form>
 		</div>
 	);
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,11 +1,25 @@
+import { Link } from 'react-router-dom';
+import Layout from '../../components/Headling/Headling';
+import Input from '../../components/InputForm/Input';
+import MaimButton from '../../components/MainButton/MainButton';
+import styles from './Register.module.css';
+
 export function Register() {
 	return (
-		<div>
-			<h1>Register</h1>
-			<form>
-				<input type='text' placeholder='Username' />
-				<input type='password' placeholder='Password' />
-				<button type='submit'>Register</button>
+		<div className={styles['login']}>
+			<Layout>Вход</Layout>
+			<form action='' className={styles['form']}>
+				<div className={styles['field']}>
+					<label htmlFor='email'>Ваш email</label>
+					<Input id='email' placeholder='Email' />
+				</div>
+				<div className={styles['field']}>
+					<label htmlFor='password'>Ваш пароль</label>
+					<Input id='password' type='password' placeholder='Пароль' />
+				</div>
+				<MaimButton appearence='big'>Вход</MaimButton>
+				<div className={styles['field']}>Нет аккаунта?</div>
+				<Link to={'/auth/register'}>Зарегистрироваться?</Link>
 			</form>
 		</div>
 	);
