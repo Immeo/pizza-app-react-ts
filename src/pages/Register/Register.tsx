6a7b4f766b8af003b96c4016826b6f11cb1685e0
immeo <wayfarer245@gmail.com>
2024-08-18 16:58:24 +0300

    39-add register logic

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,26 +1,82 @@
-import { Link } from 'react-router-dom';
-import Layout from '../../components/Headling/Headling';
+import { FormEvent, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import Headling from '../../components/Headling/Headling';
 import Input from '../../components/InputForm/Input';
 import MaimButton from '../../components/MainButton/MainButton';
-import styles from './Register.module.css';
+import { AppDispatch, RootState } from '../../store/store';
+import { register, userActions } from '../../store/user.slice';
+import styles from '../Login/Login.module.css';
+
+export type RegisterForm = {
+	email: {
+		value: string;
+	};
+	password: {
+		value: string;
+	};
+	name: {
+		value: string;
+	};
+};
 
 export function Register() {
+	const navigate = useNavigate();
+	const dispatch = useDispatch<AppDispatch>();
+	const { jwt, registrationErrorMessage } = useSelector(
+		(s: RootState) => s.user
+	);
+
+	useEffect(() => {
+		if (jwt) {
+			navigate('/');
+		}
+	}, [jwt, navigate]);
+
+	const submit = async (e: FormEvent) => {
+		e.preventDefault();
+		dispatch(userActions.clearRegistrationError());
+		const target = e.target as typeof e.target & RegisterForm;
+		const { email, password, name } = target;
+		dispatch(
+			register({
+				email: email.value,
+				password: password.value,
+				name: name.value
+			})
+		);
+	};
+
 	return (
 		<div className={styles['login']}>
-			<Layout>Вход</Layout>
-			<form action='' className={styles['form']}>
+			<Headling>Регистрация</Headling>
+			{registrationErrorMessage && (
+				<div className={styles['error']}>{registrationErrorMessage}</div>
+			)}
+			<form className={styles['form']} onSubmit={submit}>
 				<div className={styles['field']}>
 					<label htmlFor='email'>Ваш email</label>
-					<Input id='email' placeholder='Email' />
+					<Input id='email' name='email' placeholder='Email' />
 				</div>
 				<div className={styles['field']}>
 					<label htmlFor='password'>Ваш пароль</label>
-					<Input id='password' type='password' placeholder='Пароль' />
+					<Input
+						id='password'
+						name='password'
+						type='password'
+						placeholder='Пароль'
+					/>
 				</div>
-				<MaimButton appearence='big'>Вход</MaimButton>
-				<div className={styles['field']}>Нет аккаунта?</div>
-				<Link to={'/auth/register'}>Зарегистрироваться?</Link>
+				<div className={styles['field']}>
+					<label htmlFor='name'>Ваше имя</label>
+					<Input id='name' name='name' type='text' placeholder='Имя' />
+				</div>
+				<MaimButton appearence='big'>Регистрация</MaimButton>
 			</form>
+			<div className={styles['links']}>
+				<div>Есть акканут?</div>
+				<Link to='/auth/login'>Войти</Link>
+			</div>
 		</div>
 	);
 }
